2d9fe8efa4084bbd32089091386f5996b975e52c
perspect1va <gavnn31@tutamail.com>
2025-06-06 11:41:06 +0300

    laba7.py

--- laba7.py
+++ laba7.py
@@ -15,16 +15,14 @@ class CaesarEncryptor(Encryptor):
             chr((ord(c) - ord("A") + self.shift) % 26 + ord("A")) if c.isupper() else c
             for c in text
         )
-
+    
 class EncryptionManager:
     def __init__(self, encryptor: Encryptor):
         self.encryptor = encryptor
-
     def process(self, text: str) -> str:
         return self.encryptor.encrypt(text)
 
-# Использование
-text = "Hello, World!"
+text = "Hello, I'm Nick!"
 encryptor = EncryptionManager(CaesarEncryptor(shift=3))
-
+print("Исходный код:", text)
 print("Зашифрованный текст:", encryptor.process(text))
